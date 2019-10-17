cec580157fba1f2e31625d400fda09915b51616a
Tuomas Östman <tuomas.ostman@hotmail.fi>
2019-10-17 11:39:02 +0300

    Generated project

---
+++ App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreDatabaseApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
